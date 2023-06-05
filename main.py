4f0de8726f7797405086ac552e52b8e580144ea1
CurlyWendy <bubblegumsharkie@gmail.com>
2023-06-06 00:20:10 +0500

    Добавлена оценка Ходжеса-Лемана, оценка на порядковых статистиках. Добавлена модель выбросов Тьюки. Добавлено распределение Лапласа.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
 import math
+import random
 import numpy as np
-import matplotlib.pyplot as plt
 import statistics
+import matplotlib.pyplot as plt
+
 
 class RandomVariable(ABC):
     @abstractmethod
@@ -18,24 +20,53 @@ class RandomVariable(ABC):
         pass
 
 
-class NormalRandomVariable(RandomVariable):
-    def __init__(self, location=0, scale=1) -> None:
+class UniformRandomVariable(RandomVariable):
+    def __init__(self, a=0, b=1) -> None:
         super().__init__()
+        self.a = a
+        self.b = b
+
+    def pdf(self, x):
+        if x >= self.a and x <= self.b:
+            return 1 / (self.b - self.a)
+        else:
+            return 0
+
+    def cdf(self, x):
+        if x <= self.a:
+            return 0
+        elif x >= self.b:
+            return 1
+        else:
+            return (x - self.a) / (self.b - self.a)
+
+    def quantile(self, alpha):
+        return self.a + alpha * (self.b - self.a)
+
+
+class LaplaceDistribution(RandomVariable):
+    def __init__(self, location, scale):
         self.location = location
         self.scale = scale
 
     def pdf(self, x):
-        z = (x - self.location) / self.scale
-        return math.exp(-0.5 * z * z) / (math.sqrt(2 * math.pi) * self.scale)
+        return (1 / (2 * self.scale)) * np.exp(-np.abs(x - self.location) / self.scale)
 
     def cdf(self, x):
-        z = (x - self.location) / self.scale;
-        if z <= 0:
-            return 0.852 * math.exp(-math.pow((-z + 1.5774) / 2.0637, 2.34))
-        return 1 - 0.852 * math.exp(-math.pow((z + 1.5774) / 2.0637, 2.34))
+        if x < self.location:
+            return 0.5 * np.exp((x - self.location) / self.scale)
+        else:
+            return 1 - 0.5 * np.exp(-(x - self.location) / self.scale)
 
     def quantile(self, alpha):
-        return self.location + 4.91 * self.scale * (math.pow(alpha, 0.14) - math.pow(1 - alpha, 0.14))
+        if alpha < 0 or alpha > 1:
+            raise ValueError("Численное значение квантиля может быть в промежутке между 0 и 1.")
+        if alpha == 0.5:
+            return self.location
+        elif alpha < 0.5:
+            return self.location + self.scale * np.log(2 * alpha)
+        else:
+            return self.location - self.scale * np.log(2 * (1 - alpha))
 
 
 class NonParametricRandomVariable(RandomVariable):
@@ -62,6 +93,7 @@ class NonParametricRandomVariable(RandomVariable):
         index = int(alpha * len(self.source_sample))
         return self.source_sample[index]
 
+
 class RandomNumberGenerator(ABC):
     def __init__(self, random_variable: RandomVariable):
         self.random_variable = random_variable
@@ -79,10 +111,43 @@ class SimpleRandomNumberGenerator(RandomNumberGenerator):
         us = np.random.uniform(0, 1, N)
         return np.vectorize(self.random_variable.quantile)(us)
 
+
+class TukeyRandomNumberGenerator(RandomNumberGenerator):
+    def __init__(self, random_variable: RandomVariable, uniform_variable: RandomVariable, epsilon):
+        super().__init__(random_variable)
+        self.epsilon = epsilon
+        self.uniform_variable = uniform_variable
+
+    def get(self, N):
+        sample = []
+        us = np.random.uniform(0, 1, N)
+        for x in us:
+            if x < self.epsilon:
+                sample.append(self.uniform_variable.quantile(random.random()))
+            else:
+                sample.append(self.random_variable.quantile(random.random()))
+        return sample
+
+
 class Estimation(ABC):
-  @abstractmethod
-  def estimate(sample):
-    pass
+    @abstractmethod
+    def estimate(self, sample):
+        pass
+
+
+class HodgesLehmannEstimation(Estimation):
+    def estimate(self, sample):
+        n = len(sample)
+        means = [(sample[i] + sample[j]) / 2 for i in range(n) for j in range(i, n)]
+        return np.median(means)
+
+
+class HalfSumOrderStatisticsEstimation(Estimation):
+    def estimate(self, sample):
+        sorted_data = np.sort(sample)
+        r = int(len(sorted_data) / 2)
+        estimate = (sorted_data[r] + sorted_data[1 - r]) / 2
+        return estimate
 
 
 class Mean(Estimation):
@@ -90,17 +155,13 @@ class Mean(Estimation):
         return statistics.mean(sample)
 
 
-class MinMax(Estimation):
-    def estimate(self, sample):
-        return (max(sample) + min(sample)) / 2
-
-
 class Var(Estimation):
     def estimate(self, sample):
         return statistics.variance(sample)
 
+
 class Modelling(ABC):
-    def __init__(self, gen: RandomNumberGenerator, estimations: list, M:int, truth_value:float):
+    def __init__(self, gen: RandomNumberGenerator, estimations: list, M: int, truth_value: float):
         self.gen = gen
         self.estimations = estimations
         self.M = M
@@ -111,11 +172,12 @@ class Modelling(ABC):
 
     # Метод, оценивающий квадрат смещения оценок
     def estimate_bias_sqr(self):
-        return np.array([(Mean().estimate(self.estimations_sample[:,i]) - self.truth_value) ** 2 for i in range(len(self.estimations))])
+        return np.array([(Mean().estimate(self.estimations_sample[:, i]) - self.truth_value) ** 2 for i in
+                         range(len(self.estimations))])
 
     # Метод, оценивающий дисперсию оценок
     def estimate_var(self):
-        return np.array([Var().estimate(self.estimations_sample[:,i]) for i in range(len(self.estimations))])
+        return np.array([Var().estimate(self.estimations_sample[:, i]) for i in range(len(self.estimations))])
 
     # Метод, оценивающий СКО оценок
     def estimate_mse(self):
@@ -124,21 +186,24 @@ class Modelling(ABC):
     def get_samples(self):
         return self.estimations_sample
 
-    def get_sample(self):
+    def get_sample(self, N):
         return self.gen.get(N)
 
     def run(self):
         for i in range(self.M):
-            sample = self.get_sample()
+            sample = self.get_sample(N)
             self.estimations_sample[i, :] = [e.estimate(sample) for e in self.estimations]
 
+
 class SmoothedRandomVariable(RandomVariable):
+    @staticmethod
     def _k(x):
         if abs(x) <= 1:
             return 0.75 * (1 - x * x)
         else:
             return 0
 
+    @staticmethod
     def _K(x):
         if x < -1:
             return 0
@@ -160,44 +225,42 @@ class SmoothedRandomVariable(RandomVariable):
     def quantile(self, alpha):
         raise NotImplementedError
 
-location = 0 #@param {type:"number"}
-scale = 1 #@param {type:"number"}
 
-N = 500  #@param {type:"integer"}
+# location = int(input("Введите location: "))
+# scale = int(input("Введите scale: "))
+# N = int(input("Введите N: "))
+# bandwidth = float(input('Введите параметр размытости: '))
 
-rv = NormalRandomVariable(location, scale)
+location = 0
+scale = 1
+N = 500
+resample_count = 50
+bandwidth = 0.05
+
+rv = LaplaceDistribution(location, scale)
+uniformRV = UniformRandomVariable(location, scale)
 generator = SimpleRandomNumberGenerator(rv)
-
 sample = generator.get(N)
-sample
-
 rv1 = NonParametricRandomVariable(sample)
 generator1 = SimpleRandomNumberGenerator(rv1)
+generator2 = TukeyRandomNumberGenerator(rv, uniformRV, 0.5)
 
-Mean().estimate(sample)
-
-MinMax().estimate(sample)
-
-modelling = Modelling(generator1, [Mean(), MinMax()], 2 * N, location)
+modelling = Modelling(generator2, [HodgesLehmannEstimation(), HalfSumOrderStatisticsEstimation()], resample_count,
+                      location)
 modelling.run()
-
-mses = modelling.estimate_mse()
-mses
-
-mses[1] / mses[0]
-
-bandwidth = 0.05 #@param {type:"number"}
+estimate_mse = modelling.estimate_mse()
+print(estimate_mse)
+print(f'Первая оценка / Вторая оценка: {estimate_mse[0] / estimate_mse[1]}')
 
 samples = modelling.get_samples()
 POINTS = 100
 
 for i in range(samples.shape[1]):
-  sample = samples[:, i]
-  X_min = min(sample)
-  X_max = max(sample)
-  x = np.linspace(X_min, X_max, POINTS)
-  srv = SmoothedRandomVariable(sample, bandwidth)
-  y = np.vectorize(srv.pdf)(x)
-  plt.plot(x, y)
+    sample = samples[:, i]
+    X_min = min(sample)
+    X_max = max(sample)
+    x = np.linspace(X_min, X_max, POINTS)
+    srv = SmoothedRandomVariable(sample, bandwidth)
+    y = np.vectorize(srv.pdf)(x)
+    plt.plot(x, y)
 plt.show()
-
